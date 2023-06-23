02e920b104055c58e1582971ba7d8669465ff050
Achraf TAFFAH <a.taffah@etu.enset-media.ac.ma>
2023-06-23 18:45:44 +0200

    i complete create/add/display

--- Achraf-solution (correction)/polynome/main.c
+++ Achraf-solution (correction)/polynome/main.c
@@ -1,8 +1,28 @@
 #include "solution.c"
 
 int main() {
-    struct Node *newPolynome;
-    newPolynome=create_Polynome();
-    printf("coefficient:%lf  puissance:%lf",newPolynome->monome.coefficient,newPolynome->monome.puissance);
+    struct Node *head;
+    head=create_Polynome();
+
+    struct Monome monome1,monome2,monome3,monome4;
+    monome1.coefficient=(double)5;
+    monome1.puissance=(double)4;
+
+    monome2.coefficient=(double)5;
+    monome2.puissance=(double)3;
+
+    monome3.coefficient=(double)5;
+    monome3.puissance=(double)2;
+
+    monome4.coefficient=(double)5;
+    monome4.puissance=(double)1;
+
+    head=add_Monome_To_Polynome(monome1,head);
+    head=add_Monome_To_Polynome(monome2,head);
+    head=add_Monome_To_Polynome(monome3,head);
+    head=add_Monome_To_Polynome(monome4,head);
+
+    display_Polynome(head);
+
     return 0;
 }
--- Achraf-solution (correction)/polynome/solution.c
+++ Achraf-solution (correction)/polynome/solution.c
@@ -21,8 +21,38 @@ struct Node *create_Polynome(){
     newPolynome->prev=NULL;
 }
 
-struct Node *add_Monome_To_Polynome(struct Monome monome,struct Node *);
-void display_Polynome(struct Node *);
+struct Node *add_Monome_To_Polynome(struct Monome monome, struct Node *head) {
+    struct Node *temp;
+    temp = head;
+
+    if (temp->monome.coefficient == 0) {
+        temp->monome=monome;
+        return head;
+    } 
+    else {
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        
+        struct Node *newNode = create_Polynome();
+        newNode->monome = monome;
+        newNode->next = NULL;
+        newNode->prev = temp;
+        
+        temp->next = newNode;
+        return head;
+    }
+}
+
+
+void display_Polynome(struct Node *head){
+    struct Node *temp;
+    temp=head;
+    while(temp != NULL){
+        printf("%lf(X^%lf) +",temp->monome.coefficient,temp->monome.puissance);
+        temp=temp->next;
+    }
+}
 struct Node *calculate_Sum_Of_Two_Polynome(struct Node *,struct Node *);
 struct Node *calculate_Multiplication_Of_TwoPolynome(struct Node *,struct Node *);
 void solve_Polynome(struct Node *);
